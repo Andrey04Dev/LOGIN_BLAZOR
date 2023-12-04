2ea2434b27c985436f744d21a54a5d8cfdcde12e
ca_aelizondo <ca_aelizondo@conarroz.local>
2023-12-04 15:28:21 -0600

    first commit

---
+++ BlazorLogin/Server/Controllers/UsuarioController.cs
@@ -0,0 +1,34 @@
+﻿using BlazorLogin.Shared;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlazorLogin.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsuarioController : ControllerBase
+    {
+        [HttpPost]
+        [Route("Login")]
+
+        public async Task<IActionResult> Login([FromBody] LoginDTO login)
+        {
+            SesionDTO sesionDTO = new SesionDTO();
+            if (login.Correo == "admin@gmail.com" && login.Clave == "admin")
+            {
+                sesionDTO.Nombre = "Admin";
+                sesionDTO.Correo = login.Correo;
+                sesionDTO.Rol = "Administrador";
+
+            }
+            else
+            {
+                sesionDTO.Nombre = "Empleado";
+                sesionDTO.Correo = login.Correo;
+                sesionDTO.Rol = "Empleado";
+            }
+
+            return StatusCode(statusCode: 200, sesionDTO);
+        }
+    }
+}
